6dd8a019e6b2b34e6e040c4507aeb7d54e0da8e4
Anav Jain <anavjain6@gmail.com>
2024-10-28 21:30:24 +0530

    Update TooltipPortal.js

--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -1,9 +1,23 @@
+// components/TooltipPortal.js
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
+const TooltipPortal = ({ content }) => {
+    return ReactDOM.createPortal(
+        <div style={tooltipStyles}>
+            {content}
+        </div>,
+        document.body // Render the tooltip in the body
+    );
+};
 
-function TooltipPortal({ children }) {
-
-}
+const tooltipStyles = {
+    position: 'absolute',
+    backgroundColor: 'black',
+    color: 'white',
+    padding: '5px 10px',
+    borderRadius: '5px',
+    zIndex: 1000,
+};
 
 export default TooltipPortal;
