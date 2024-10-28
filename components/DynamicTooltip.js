e39f53820afa4843cc1a777ea6fd70ed10cde36b
Anav Jain <anavjain6@gmail.com>
2024-10-28 21:30:10 +0530

    Update DynamicTooltip.js

--- components/DynamicTooltip.js
+++ components/DynamicTooltip.js
@@ -1,18 +1,13 @@
-import { useState, useEffect } from 'react';
+// components/DynamicTooltip.js
+import React from 'react';
 import TooltipPortal from './TooltipPortal';
 
-function DynamicTooltip() {
-
-
- 
-
-  return (
-    <TooltipPortal>
-      <div style={{ position: 'absolute', top: '50px', left: '50px', backgroundColor: 'gray', padding: '10px', borderRadius: '5px' }}>
-   
-      </div>
-    </TooltipPortal>
-  );
-}
+const DynamicTooltip = ({ isVisible }) => {
+    return (
+        <>
+            {isVisible && <TooltipPortal content="This is a tooltip" />}
+        </>
+    );
+};
 
 export default DynamicTooltip;
